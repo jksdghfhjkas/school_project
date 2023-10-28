1738cbbe197f09cba76f1d10415ff35e6a8d1421
Eltrox <erem_1956@mail.ru>
2023-10-28 19:13:49 +0900

    one

--- weather/test.py
+++ weather/test.py
@@ -1,6 +1,29 @@
 import requests
 import asyncio
+import json
+
+URL = ''
+
+USER_SITY = []
 
 
+async def Request_Get_Weather(city, session):
+    async with session.get(URL.format(city), proxy=None) as response:
+        response_json = json.loads(await response.text())
+        
+        if response_json['cod'] != '404':
+
+            USER_SITY.append({
+                'city': city,
+                'temp': response_json['main']['temp'],
+                'image': response_json['weather'][0]['icon']
+            })
+
+async def Request_User_Weather(cityes):
+    city_tasks = []
+    async with aiohttp.ClientSession() as session:
+        for city in cityes:
+            city_tasks.append(asyncio.create_task(Request_Get_Weather(city, session)))
+        await asyncio.gather(*city_tasks)
 
 
