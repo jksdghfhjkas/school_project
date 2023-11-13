65a242acb56fa25a22378515c42f52f8632d6c62
EltroxHelpRegister <121621610+jksdghfhjkas@users.noreply.github.com>
2023-11-14 00:55:04 +0900

    Update settings.py

--- weather/weather/settings.py
+++ weather/weather/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-znhr*(v#mip4dy8-dgq!64+cot%3pkjvf8mv-za2t9xtxglvkw'
+SECRET_KEY = 'django-secret-key'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
